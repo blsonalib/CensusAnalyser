6a756c9278b823f4c54b50b1a96b7c8dc69a7831
blsonalib <blsonalib@gmail.com>
2019-12-09 16:07:13 +0530

    Refactored to get list as well as iterator

--- src/main/java/censusanalyser/CSVBuilderException.java
+++ src/main/java/censusanalyser/CSVBuilderException.java
@@ -2,14 +2,12 @@ package censusanalyser;
 
 public class CSVBuilderException extends Exception {
 
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE;
+    }
+    ExceptionType type;
     public CSVBuilderException(String message, ExceptionType type) {
         super(message);
-        this.type=type;
+        this.type = type;
     }
-
-    enum ExceptionType {
-        CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE;
-    }
-
-    ExceptionType type;
 }
--- src/main/java/censusanalyser/OpenCsvBuilder.java
+++ src/main/java/censusanalyser/OpenCsvBuilder.java
@@ -6,17 +6,28 @@ import com.sun.tools.jdeprscan.CSV;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCsvBuilder<E> implements ICSVBuilder{
+
+
     public  Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
-        try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
-        } catch (IllegalStateException e) {
-            throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
-        }
+            return this.getCSVBean(reader, csvClass).iterator();
+
     }
+    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+
+            return this.getCSVBean(reader, csvClass).parse();
+    }
+    private CsvToBean<E>getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException{
+            try {
+                CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(csvClass);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+                return csvToBeanBuilder.build();
+            } catch (IllegalStateException e) {
+                throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
+            }
+        }
 }
