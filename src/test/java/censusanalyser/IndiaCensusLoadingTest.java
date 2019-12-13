cbc3b738fcfde76dc620010f0c7b6084f9695435
blsonalib <blsonalib@gmail.com>
2019-12-13 17:34:37 +0530

    Added test case for without header in indiaStatedataCsv by using adaptor in IndiaCensusLoadingTest

--- src/test/java/censusanalyser/IndiaCensusLoadingTest.java
+++ src/test/java/censusanalyser/IndiaCensusLoadingTest.java
@@ -80,4 +80,16 @@ public class IndiaCensusLoadingTest {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.ISSUE_IN_FILE, e.type);
         }
     }
+
+    @Test
+    public void givenIndiaStateCodeData_WithoutHeader_ShouldReturnThrowException() {
+        try {
+            CensusAdapter indiaCensusAdapter = new IndiaCensusAdapter();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            indiaCensusAdapter.loadCensusData(INDIA_STATECODE_CSV_FILE_PATH_FOR_HEADER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.ISSUE_IN_FILE, e.type);
+        }
+    }
 }
