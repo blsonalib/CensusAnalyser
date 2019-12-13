45e39ba425890b4a415a0d7196483fc32752b0cf
blsonalib <blsonalib@gmail.com>
2019-12-13 19:35:26 +0530

    Added new method for comparing most populous state with dencity

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -1,20 +1,19 @@
 package censusanalyser;
-
 import com.google.gson.Gson;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class CensusAnalyser {
 
     public enum Country {INDIA, US}
+
     private Country country;
 
     Map<String, CensusDAO> censusStateMap = null;
 
-    public CensusAnalyser(Country country){
+    public CensusAnalyser(Country country) {
 
-        this.country=country;
+        this.country = country;
     }
 
     public int loadCensusData(Country country, String... csvFilePath) throws CensusAnalyserException {
@@ -22,18 +21,31 @@ public class CensusAnalyser {
         return censusStateMap.size();
     }
 
-    public String  getStateWiseSortedCensusData(SortedField.Field field) throws CensusAnalyserException {
-        if(censusStateMap==null || censusStateMap.size()==0)
-        {
-            throw new CensusAnalyserException("No Census data",CensusAnalyserException.ExceptionType.No_CENSUS_DATA);
+    public String getStateWiseSortedCensusData(SortedField.Field field) throws CensusAnalyserException {
+        if (censusStateMap == null || censusStateMap.size() == 0) {
+            throw new CensusAnalyserException("No Census data", CensusAnalyserException.ExceptionType.No_CENSUS_DATA);
         }
-        Comparator<CensusDAO> censusCSVComparator=SortedField.getField(field);
-        ArrayList censusDAOS=censusStateMap.values().stream().
+        Comparator<CensusDAO> censusCSVComparator = SortedField.getField(field);
+        ArrayList censusDAOS = censusStateMap.values().stream().
                 sorted(censusCSVComparator).
                 map(censusDAO -> censusDAO.getCensusDTO(country)).
                 collect(Collectors.toCollection(ArrayList::new));
-        String sortedStateCensusJson=new Gson().toJson(censusDAOS);
+        String sortedStateCensusJson = new Gson().toJson(censusDAOS);
         return sortedStateCensusJson;
     }
+
+    public String getStateWiseSortedByTwoField(SortedField.Field field,SortedField.Field field2) throws CensusAnalyserException {
+        if (censusStateMap == null || censusStateMap.size() == 0) {
+            throw new CensusAnalyserException("No Census data", CensusAnalyserException.ExceptionType.No_CENSUS_DATA);
+        }
+        Comparator<CensusDAO> censusCSVComparator = SortedField.getField(field).thenComparing(SortedField.getField(field2));
+        ArrayList censusDAOS = censusStateMap.values().stream().
+                sorted(censusCSVComparator).
+                map(censusDAO -> censusDAO.getCensusDTO(country)).
+                collect(Collectors.toCollection(ArrayList::new));
+        String sortedStateCensusJson = new Gson().toJson(censusDAOS);
+        return sortedStateCensusJson;
+    }
+
 }
 
